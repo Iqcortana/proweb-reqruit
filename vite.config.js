98c2481dbeb2c4ec2b63753af6556b3e58548125
IqCortana <faiq.id2017@gmail.com>
2024-05-31 14:20:55 +0800

    tambahan vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build', // Output directory for build files
+    rollupOptions: {
+      input: {
+        main: 'resources/js/app.js', // Entry point for your application
+      },
+    },
+  },
 });
+
